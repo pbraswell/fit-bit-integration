92ef5afe6e73928d7a671cb110f3e80fdcbdca7b
pbraswell <peter.braswell@gmail.com>
2014-05-13 14:36:30 -0400

    added responder for controller interface and added a restful API for by date retrieval

--- app/controllers/sleep_logs_controller.rb
+++ app/controllers/sleep_logs_controller.rb
@@ -1,10 +1,21 @@
 class SleepLogsController < ApplicationController
+  respond_to :html, :xml, :json
+
   def index
   end
 
   def show
-    logger.info "fetch sleep record for: #{params[:username]}"
+    logger.info "fetch sleep log for: #{params[:username]}"
     @user = User.find_by_username params[:username]
     @sleep_log = @user.get_todays_sleep_log unless @user.nil?
+    respond_with(@sleep_log)
   end
+
+  def get_by_date
+    logger.info "fetch sleep log by user name date"
+    @user = User.find_by_username params[:username]
+    @sleep_log = @user.get_todays_sleep_log unless @user.nil?
+    respond_with(@sleep_log)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FitBitIntegration::Application.routes.draw do
   
   get 'sleep_logs/index'
+  get 'sleep_logs/:username/:date', to: 'sleep_logs#get_by_date'
   post 'sleep_logs/show'
 
   get 'authorization/auth_fitbit'
