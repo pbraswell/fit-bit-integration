0d21069626785d878a3b485f9f18302154efa6c4
pbraswell <peter.braswell@gmail.com>
2014-05-14 15:07:26 -0400

    save before branching

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,8 +1,11 @@
 class AuthorizationController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :auth_fitbit
+
   def auth_fitbit
     session[:external_user] = params[:external_user]
     session[:return_to] ||= request.referer
     redirect_to '/auth/fitbit/'
   end
+
+  
 end
